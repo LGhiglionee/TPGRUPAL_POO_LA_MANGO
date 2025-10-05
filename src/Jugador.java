66253c5fda5dcfc972366298ca883ef39f417746
lbendersky <lbendersky@uade.edu.ar>
2025-10-05 10:56:20 -0300

    Agregue clase Jugador

---
+++ src/Jugador.java
@@ -0,0 +1,25 @@
+public class Jugador {
+    private int salud;
+    private int mana;
+
+    public Jugador() {
+        this.salud = 100;
+        this.mana = 0;
+    }
+
+    public void reducirSalud(int danio) {
+        this.salud -= danio;
+    }
+
+    public void agregarSalud(int curacion) {
+        this.salud += curacion;
+    }
+
+    public void reducirMana(int mana) {
+        this.mana += mana;
+    }
+
+    public void agregarMana(int mana) {
+        this.mana += mana;
+    }
+}
