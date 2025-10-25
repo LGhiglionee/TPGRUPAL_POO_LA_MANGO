e48768b87e854d1ed7ab73e0ec7a7f6a4b41e3d7
Gaspar Labastie <110985466+Gaspilabastie@users.noreply.github.com>
2025-10-25 15:54:10 -0300

    .

--- src/Menu2.java
+++ src/Menu2.java
@@ -20,38 +20,50 @@ public class Menu2 {
 class Inicio extends JFrame implements ActionListener {
     JButton botonjugar;
     JButton botonsalir;
+    JLabel titulo;
     public Inicio() {
+        setTitle("Truco a 2 Lucas");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         //Creacion del marco principal
         Toolkit mipantalla =  Toolkit.getDefaultToolkit();
         Dimension tamanio = mipantalla.getScreenSize();
         int altura = tamanio.height;
         int anchura = tamanio.width;
-        setBounds(anchura/4,altura/4 ,anchura/2 ,altura/2 );
 
-        //Titulo del programa e icono
-        setTitle("Truco a 2 Lucas");
-        Image fondo=mipantalla.getImage("Imagenes/download.jpeg");
+        //Tamaño de pnatalla
+        setBounds(anchura / 4, altura / 4, anchura / 2, altura / 2);
+
+        //Icono del programa
+        Image fondo = mipantalla.getImage("Imagenes/Fondojuego.png");
         setIconImage(fondo);
 
-        //Botones de jugar y salir
-        int anchoboton = anchura/10;
-        int  altoboton = altura/16;
-
-
-        botonjugar = new JButton("Jugar");
-        botonjugar.setBounds(anchura/4 - anchoboton/2,altura/4,anchoboton,altoboton);
-        botonjugar.addActionListener(this);
-        add(botonjugar);
-
-        botonsalir = new JButton("Salir");
-        botonsalir.setBounds(anchura/4 - anchoboton/2,altura/4 + 100,anchoboton,altoboton);
-        botonsalir.addActionListener(this);
-        add(botonsalir);
-
-        //Lamina
+        //lamina
         Lamina lamina = new Lamina();
-        add(lamina);
+        lamina.setLayout(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.insets = new Insets(10, 10, 10, 10);
+        gbc.gridx = 0;
+        gbc.gridy = 0;
 
+        //Titulo
+        titulo = new  JLabel("Truco a 2 lucas");
+        titulo.setFont(new Font("Arial", Font.BOLD, 50));
+        titulo.setForeground(Color.BLUE);
+        lamina.add(titulo,gbc);
+
+        //Botones de jugar y salir
+        gbc.gridy = 1;
+        botonjugar = new JButton("Jugar");
+        botonjugar.addActionListener(this);
+        lamina.add(botonjugar,gbc);
+
+        gbc.gridy = 2;
+        botonsalir = new JButton("Salir");
+        botonsalir.addActionListener(this);
+        lamina.add(botonsalir,gbc);
+
+        add(lamina);
+        setVisible(true);
     }
 
     public void actionPerformed(ActionEvent e) {
@@ -68,63 +80,83 @@ class Inicio extends JFrame implements ActionListener {
 class Lamina extends JPanel {
     private Image imagen;
 
+    //Imagen de fondo
+
+    public Lamina() {
+        File miimagen = new File("Imagenes/Fondojuego.png");
+        try {
+            imagen = ImageIO.read(miimagen);
+        } catch (IOException e) {
+            System.out.println("La imagen no se encuentra");
+        }
+    }
+    @Override
     public void paintComponent(Graphics g) {
         //Cosas de la funcion
         super.paintComponent(g);
-        Graphics2D g2 = (Graphics2D) g;
 
-        //Imagen de fondo
-        File miimagen=new File("Imagenes/BarMenuPrincipal.jpeg");
-        try {
-            imagen = ImageIO.read(miimagen);
+        if (imagen != null) {
+            Graphics2D g2 = (Graphics2D) g;
+            g2.drawImage(imagen, 0, 0, getWidth(),getHeight(),this);
         }
-        catch(IOException e) {
-            System.out.println("La imagen no se encuentra");
-        }
-        Image imagencompleta = imagen.getScaledInstance(getWidth(), getHeight(), Image.SCALE_SMOOTH);
-        g2.drawImage(imagencompleta,0,0,null);
 
-        //Fuente de la letra del titulo y tamaño
-        Toolkit mipantalla =  Toolkit.getDefaultToolkit();
-        Dimension tamanio = mipantalla.getScreenSize();
-        int altura = tamanio.height;
-        int anchura = tamanio.width;
-
-        Font mifuente = new Font("Arial", Font.BOLD, 50);
-        g2.setFont(mifuente);
-        g2.setColor(Color.BLUE);
-        g2.drawString("Truco a dos Lucas", anchura/9, altura/8);
 
     }
 }
 
 class Partida extends JFrame implements ActionListener {
+    //Botones de cartas
     JButton botoncarta1;
     JButton botoncarta2;
     JButton botoncarta3;
+
+    //Información de jugadores
     JProgressBar j1salud;
     JProgressBar j2salud;
     JLabel j1mana;
     JLabel j2mana;
+    JLabel j1nombre;
+    JLabel j2nombre;
     JLabel jturno;
+
+    //Paneles
+    JPanel j1Info;
+    JPanel j2Info;
+    JPanel manoCartas;
+    JPanel infoTurno;
+
     Turnos turno;
     ArrayList<Carta> cartasjugadas;
 
     public Partida() {
         //Creacion del marco principal
-        Toolkit mipantalla =  Toolkit.getDefaultToolkit();
+        Toolkit mipantalla = Toolkit.getDefaultToolkit();
         Dimension tamanio = mipantalla.getScreenSize();
         int altura = tamanio.height;
         int anchura = tamanio.width;
-        setBounds(anchura/4,altura/4 ,anchura/2 ,altura/2 );
+        setBounds(anchura / 4, altura / 4, anchura / 2, altura / 2);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         //Titulo del programa e icono
         setTitle("Truco a 2 Lucas");
-        Image fondo=mipantalla.getImage("Imagenes/download.jpeg");
+        Image fondo = mipantalla.getImage("Imagenes/Fondojuego.png");
         setIconImage(fondo);
 
+        //inicializar datos
+        turno = new Turnos();
+        j1salud = new JProgressBar(0, 100);
+        j2salud = new JProgressBar(0, 100);
+        j1mana = new JLabel("Mana Jugador 1: " + turno.getJugador1().getMana());
+        j1mana.setForeground(Color.WHITE);
+        j2mana = new JLabel("Mana Jugador 2: " + turno.getJugador2().getMana());
+        j2mana.setForeground(Color.WHITE);
+        jturno = new JLabel("Turno de Jugador 1");
+
+        botoncarta1 = new JButton("Carta 1");
+        botoncarta2 = new JButton("Carta 2");
+        botoncarta3 = new JButton("Carta 3");
+
         //Inicializacion jugadores y turno
-        turno =  new Turnos();
         turno.llenarMano(turno.getJugador1().getTresCartas(), turno.getJugador1());
         turno.llenarMano(turno.getJugador2().getTresCartas(), turno.getJugador2());
         cartasjugadas =  new ArrayList<Carta>();
@@ -133,83 +165,118 @@ class Partida extends JFrame implements ActionListener {
         int anchoboton = anchura/12;
         int  altoboton = altura/6;
 
-        //Barra salud
+        //Obtener cartas
+        Carta carta1 = turno.getJugadorMano().getTresCartas().get(0);
+        Carta carta2 = turno.getJugadorMano().getTresCartas().get(1);
+        Carta carta3 = turno.getJugadorMano().getTresCartas().get(2);
 
-        j1salud = new JProgressBar(0,100);
-        j1salud.setBounds(anchura/4 - anchoboton*2, altura/50,115,20);
+        //Nombre jugador
+        j1nombre = new JLabel();
+        j2nombre = new JLabel();
+
+        // Salud Jugador 1
+        j1salud = new JProgressBar(0, 100);
         j1salud.setValue(turno.getJugador1().getSalud());
         j1salud.setStringPainted(true);
         j1salud.setForeground(Color.RED);
         j1salud.setString("Vida: " + turno.getJugador1().getSalud());
 
-        j2salud = new JProgressBar(0,100);
-        j2salud.setBounds(anchura/4 + anchoboton,altura/50,115,20);
+        // Salud J2
+        j2salud = new JProgressBar(0, 100);
         j2salud.setValue(turno.getJugador2().getSalud());
         j2salud.setStringPainted(true);
         j2salud.setForeground(Color.RED);
         j2salud.setString("Vida: " + turno.getJugador2().getSalud());
 
-
-        //Etiquetas mana
-        j1mana = new JLabel("Mana Jugador 1: " + turno.getJugador1().getMana());
-        j1mana.setBounds(anchura/4 - anchoboton*2, altura/50,anchura/6,altura/10);
-
-        j2mana = new JLabel("Mana Jugador 2: " + turno.getJugador2().getMana());
-        j2mana.setBounds(anchura/4 + anchoboton,altura/50,anchura/6,altura/10);
-
-        //Etiqueta turno
-        jturno = new JLabel("Turno de Jugador 1");
-        jturno.setBounds(anchura/9, altura/75, anchura/2,altura/4);
+        //Turno
         Font mifuente = new Font("Arial", Font.BOLD, 45);
         jturno.setFont(mifuente);
-        jturno.setForeground(Color.BLUE);
+        jturno.setForeground(Color.WHITE);
 
-        //Botones de cartas
-
-        Carta carta1 = new Carta();
-        Carta carta2 = new Carta();
-        Carta carta3 = new Carta();
-
-        carta1 = turno.getJugadorMano().getTresCartas().get(0);
-        carta2 = turno.getJugadorMano().getTresCartas().get(1);
-        carta3 = turno.getJugadorMano().getTresCartas().get(2);
-
+        //Cratas
         ImageIcon imgcarta1 = new ImageIcon(carta1.getImagen());
-        //Image imagencompleta1 = imgcarta1.getImage().getScaledInstance(getWidth(), getHeight(), Image.SCALE_SMOOTH);
-        botoncarta1 = new JButton(imgcarta1);
-        botoncarta1.setBounds(anchura/4 - anchoboton*2,altura/4,anchoboton,altoboton);
+        Image img1 = imgcarta1.getImage().getScaledInstance(anchoboton, altoboton, Image.SCALE_SMOOTH);
+        botoncarta1 = new JButton(new ImageIcon(img1));
         botoncarta1.addActionListener(this);
-        add(botoncarta1);
 
         ImageIcon imgcarta2 = new ImageIcon(carta2.getImagen());
-        botoncarta2 = new JButton(imgcarta2);
-        botoncarta2.setBounds(anchura/4 - anchoboton/2,altura/4,anchoboton,altoboton);
+        Image img2 = imgcarta2.getImage().getScaledInstance(anchoboton, altoboton, Image.SCALE_SMOOTH);
+        botoncarta2 = new JButton(new ImageIcon(img2));
         botoncarta2.addActionListener(this);
-        add(botoncarta2);
 
         ImageIcon imgcarta3 = new ImageIcon(carta3.getImagen());
-        botoncarta3 = new JButton(imgcarta3);
-        botoncarta3.setBounds(anchura/4 + anchoboton,altura/4,anchoboton,altoboton);
+        Image img3 = imgcarta3.getImage().getScaledInstance(anchoboton, altoboton, Image.SCALE_SMOOTH);
+        botoncarta3 = new JButton(new ImageIcon(img3));
         botoncarta3.addActionListener(this);
-        add(botoncarta3);
 
-        //Lamina
+        //Panel Principal
         Juego juego = new Juego();
-        juego.setBounds(0,0,anchura,altura);
+        juego.setLayout(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.insets = new Insets(10, 10, 10, 10);
 
-        JLayeredPane vista = new JLayeredPane();
-        vista.setBounds(0,0,getWidth(),getHeight());
-        vista.add(juego, JLayeredPane.DEFAULT_LAYER);
-        vista.add(jturno, JLayeredPane.PALETTE_LAYER);
-        vista.add(j1salud,  JLayeredPane.PALETTE_LAYER);
-        vista.add(j2salud,  JLayeredPane.PALETTE_LAYER);
-        vista.add(j1mana,   JLayeredPane.PALETTE_LAYER);
-        vista.add(j2mana,   JLayeredPane.PALETTE_LAYER);
-        vista.add(botoncarta1,  JLayeredPane.PALETTE_LAYER);
-        vista.add(botoncarta2,  JLayeredPane.PALETTE_LAYER);
-        vista.add(botoncarta3,  JLayeredPane.PALETTE_LAYER);
+        //Creacion de sub-paneles
+        j1Info = new JPanel();
+        j1Info.setOpaque(false);
+        j2Info = new JPanel();
+        j2Info.setOpaque(false);
+        infoTurno = new JPanel();
+        infoTurno.setOpaque(false);
+        manoCartas = new JPanel();
+        manoCartas.setOpaque(false);
 
-        add(vista);
+        //Borde de los sub-paneles
+        j1Info.setBorder(BorderFactory.createTitledBorder("Jugador 1"));
+        j2Info.setBorder(BorderFactory.createTitledBorder("Jugador 2"));
+
+        //Organiza sub-paneles
+        j1Info.setLayout(new GridLayout(3,1));
+        j1Info.add(j1nombre);
+        j1Info.add(j1salud);
+        j1Info.add(j1mana);
+
+        j2Info.setLayout(new GridLayout(3,1));
+        j2Info.add(j2nombre);
+        j2Info.add(j2salud);
+        j2Info.add(j2mana);
+
+        infoTurno.add(jturno);
+
+        manoCartas.add(botoncarta1);
+        manoCartas.add(botoncarta2);
+        manoCartas.add(botoncarta3);
+
+        //Panel jugador 1 (Arriba - izquierda)
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.weightx = 0.1;
+        gbc.anchor = GridBagConstraints.NORTHWEST;
+        juego.add(j1Info, gbc);
+
+        //Panel turno (Arriba - Centro)
+        gbc.gridx = 1;
+        gbc.gridy = 0;
+        gbc.weightx = 0.8;
+        gbc.anchor = GridBagConstraints.NORTH;
+        juego.add(infoTurno, gbc);
+
+        //Panel jugador 2 (Arriba - derecha)
+        gbc.gridx = 2;
+        gbc.gridy = 0;
+        gbc.weightx = 0.1;
+        gbc.anchor = GridBagConstraints.NORTHEAST;
+        juego.add(j2Info, gbc);
+
+        //Panel mano (Abajo)
+        gbc.gridx = 0;
+        gbc.gridy = 1;
+        gbc.gridwidth = 3;
+        gbc.weighty = 1.0;
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        gbc.anchor = GridBagConstraints.SOUTH;
+        juego.add(manoCartas, gbc);
+
+        add(juego);
         setVisible(true);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
@@ -290,7 +357,7 @@ class Juego extends JPanel {
         Graphics2D g2 = (Graphics2D) g;
 
         //Imagen de fondo
-        File miimagen=new File("Imagenes/CartasFondo.jpg");
+        File miimagen=new File("Imagenes/FondoJuego.png");
         try {
             imagen = ImageIO.read(miimagen);
         }
@@ -301,14 +368,9 @@ class Juego extends JPanel {
         g2.drawImage(imagencompleta,0,0,null);
 
         //Fuente de la letra del titulo
-        /*Toolkit mipantalla =  Toolkit.getDefaultToolkit();
+        Toolkit mipantalla =  Toolkit.getDefaultToolkit();
         Dimension tamanio = mipantalla.getScreenSize();
         int altura = tamanio.height;
         int anchura = tamanio.width;
-
-        Font mifuente = new Font("Arial", Font.BOLD, 50);
-        g2.setFont(mifuente);
-        g2.setColor(Color.BLUE);
-        g2.drawString("Turno de " + turnoActual, anchura/9, altura/5);*/
     }
 }
