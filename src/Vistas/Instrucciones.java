7013bf62bdcfaf3ddaa84620de9ed757f267283e
Gaspar Labastie <110985466+Gaspilabastie@users.noreply.github.com>
2025-10-30 17:00:31 -0300

    Meojora de instrucciones

--- src/Vistas/Instrucciones.java
+++ src/Vistas/Instrucciones.java
@@ -3,7 +3,11 @@ package Vistas;
 import Modelo.GestorRecursos;
 
 import javax.swing.*;
+import javax.swing.text.SimpleAttributeSet;
+import javax.swing.text.StyleConstants;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
@@ -13,10 +17,11 @@ import java.io.IOException;
  * Lee el contenido del archivo de texto {@code instrucciones.txt} ubicado en la carpeta de recursos.
  */
 
-class Instrucciones extends JFrame {
+class Instrucciones extends JFrame implements  ActionListener {
     /**
      * Constructor que inicializa la ventana y carga el texto de instrucciones.
      */
+    JButton botonVolver;
     public Instrucciones() {
         // --- Configuración general de la pantalla.
         Toolkit mipantalla = Toolkit.getDefaultToolkit();
@@ -30,10 +35,10 @@ class Instrucciones extends JFrame {
 
         // --- Carga de fuente personalizada.
         Font fuente = GestorRecursos.cargarFuente("src/Recursos/Fuentes/ka1.ttf");
-        Font fuenteTexto = fuente.deriveFont(Font.BOLD, 10f);
+        Font fuenteTexto = fuente.deriveFont(Font.BOLD, 13f);
 
         // --- Lamina
-        PanelConFondo lamina = new PanelConFondo("src/Recursos/Imagenes/FondoMenu.png");
+        PanelConFondo lamina = new PanelConFondo("src/Recursos/Imagenes/FondoInstrucciones.png");
         lamina.setLayout(new GridBagLayout());
         GridBagConstraints gbc = new GridBagConstraints();
 
@@ -42,17 +47,53 @@ class Instrucciones extends JFrame {
         texto.setFont(fuenteTexto);
         texto.setEditable(false);
         texto.setOpaque(false);
+        SimpleAttributeSet attribs = new SimpleAttributeSet();
+        StyleConstants.setAlignment(attribs, StyleConstants.ALIGN_CENTER);
+        texto.setParagraphAttributes(attribs, false);
 
         JScrollPane scroll = new JScrollPane(texto);
         add(scroll, BorderLayout.CENTER);
+
+        // --- Boton volver
+        botonVolver = new JButton("Volver");
+        botonVolver.addActionListener( this);
+        ImageIcon imagenBoton = GestorRecursos.cargarImagenEscalada("src/Recursos/Imagenes/FondoBoton.png", anchura / 7, altura / 12);
+        botonVolver.setHorizontalTextPosition(SwingConstants.CENTER);
+        botonVolver.setVerticalTextPosition(SwingConstants.CENTER);
+        botonVolver.setIcon(imagenBoton);
+        botonVolver.setBorderPainted(false);
+        botonVolver.setContentAreaFilled(false);
+        botonVolver.setFont(fuenteTexto);
 
         // --- Carga del archivo de instrucciones
         String ruta = "src/Recursos/Instrucciones/instrucciones.txt";
         texto.setText(cargarTextoDesdeArchivo(ruta));
 
+        // --- Logica gbc - Lamina
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.weightx = 1;
+        gbc.insets = new Insets(100, 500, 10, 450);
+        gbc.fill = GridBagConstraints.BOTH;
+        gbc.anchor = GridBagConstraints.CENTER;
+        lamina.add(texto,gbc);
+
+        gbc.gridy = 1;
+        gbc.weightx = 1;
+        gbc.insets = new Insets(10, 200, 10, 200);
+        gbc.anchor = GridBagConstraints.SOUTH;
+        lamina.add(botonVolver,gbc);
+
+        add(lamina);
         setVisible(true);
     }
 
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == botonVolver) {
+            new Inicio();
+            dispose();
+        }
+    }
     /**
      * Lee el contenido de un archivo de texto y lo devuelve como cadena.
      */
